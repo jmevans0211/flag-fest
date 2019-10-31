3091399f948fb0564ed3605f88363f72c74fa38c
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-30 21:37:23 -0600

    Change state of region when the Asia, the Americas, or Europe  image is clicked

--- src/components/ChooseRegion/ChooseRegion.js
+++ src/components/ChooseRegion/ChooseRegion.js
@@ -6,14 +6,20 @@ class ChooseRegion extends Component {
   constructor() {
     super();
     this.state = {
-      region: '',
+      region: [],
       tenLimit: false,
     }
   }
 
   handleClick = (e, region) => {
     if (region === 'africa') {
-      this.setState({ region: 'Africa'})
+      this.setState({ region: ['Africa']})
+    } else if (region === 'europe') {
+      this.setState({ region: ['Europe']})
+    } else if (region === 'asia-oceania') {
+      this.setState({ region: ['Asia', 'Oceania'] })
+    } else if (region === 'americas') {
+      this.setState({ region: ['America']})
     }
   }
 
@@ -22,10 +28,10 @@ class ChooseRegion extends Component {
     return (
       <main>
         <h1>Choose a Region</h1>
-        <img id="africa" className="region" onClick={(e) => this.handleClick(e, 'africa')} src="https://svgsilh.com/svg/151640.svg" />
-        <img id="europe" className="region" src="https://svgsilh.com/svg/151641.svg" />
-        <img id="asia-aus" className="region" src="https://svgsilh.com/svg/307197.svg" />
-        <img id="americas" className="region" src="https://svgsilh.com/svg_v2/714733.svg" />
+        <img className="region" onClick={(e) => this.handleClick(e, 'africa')} src="https://svgsilh.com/svg/151640.svg" />
+        <img className="region" onClick={(e) => this.handleClick(e, 'europe')} src="https://svgsilh.com/svg/151641.svg" />
+        <img className="region" onClick={(e) => this.handleClick(e, 'asia-oceania')} src="https://svgsilh.com/svg/307197.svg" />
+        <img className="region" onClick={(e) => this.handleClick(e, 'americas')} src="https://svgsilh.com/svg_v2/714733.svg" />
         <div className="flag-amount-container">
           <h4>Choose How Many Flags You'd Like To Be Tested On</h4>
           <button type="button">All Flags for this Region</button>

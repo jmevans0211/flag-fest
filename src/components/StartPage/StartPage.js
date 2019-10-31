bea3c327145718bca9a903a6989f0e46ae60e887
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-10-30 22:21:52 -0600

    Minor styling and alignment for StartPage

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -5,9 +5,12 @@ import './StartPage.scss'
 const StartPage = () => {
   return (
     <div className="start-container">
-      <Link to='/flag-fest' >
-        <h3 className="start-button" role="button">START GAME</h3>
-      </Link>
+      <div className="start-content">
+        <h1>Flag Fest</h1>
+        <Link to='/flag-fest' >
+          <h3 className="start-button" role="button">START GAME</h3>
+        </Link>
+      </div>
     </div>
   )
 

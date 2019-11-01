167481b8b8f46673b883791d9fe2aeef2ccfc54b
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-01 11:55:05 -0600

    Set up actions and reducers to remove guessed flag country from store

--- src/reducers/countries.js
+++ src/reducers/countries.js
@@ -2,6 +2,8 @@ export const countries = (state=[], action) => {
   switch(action.type) {
     case 'SAVE_COUNTRIES':
       return action.countries
+    case 'REMOVE_COUNTRY_GUESSED':
+      return state.unshift()
     default:
       return state
   }

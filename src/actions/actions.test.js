8d0e7565c11c2c5cf83b6a61f18b87652cf22fce
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-02 14:27:51 -0600

    Add actions test for saveCountries

---
+++ src/actions/actions.test.js
@@ -0,0 +1,32 @@
+import * as actions from '../actions';
+
+describe('actions', () => {
+  it('should have a type of SAVE_COUNTRIES', () => {
+    const countries = [
+      {
+        flag: "https://restcountries.eu/data/srb.svg",
+        name: "Serbia",
+        numericCode: 688,
+        region: "Europe"
+      },
+      {
+        flag: "https://restcountries.eu/data/lux.svg",
+        name: "Luxembourg",
+        numericCode: 442,
+        region: "Europe"
+      }
+    ]
+    
+    const expectedAction = {
+      type: 'SAVE_COUNTRIES',
+      countries: countries
+    }
+
+    const result = actions.saveCountries(countries)
+
+    expect(result).toEqual(expectedAction)
+  });
+
+
+
+});

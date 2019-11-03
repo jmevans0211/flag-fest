97ee4dc0975d0abf9f2a5f0acb6d1ad4416c6e14
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-03 14:15:14 -0700

    Add sad path test

--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -26,5 +26,17 @@ describe('fetchData', () => {
     expect(results).toEqual(mockData)
   });
 
+  it('should return an error', () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: false,
+        statusText: "Invalid API key: You must be granted a valid key."
+      })
+    });
+
+    const mockUrl = 'https://kittens.com';
+
+    expect(fetchData(mockUrl)).rejects.toEqual(Error("Invalid API key: You must be granted a valid key."));
+  });
 
 });

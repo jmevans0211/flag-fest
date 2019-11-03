b7b8ef66ceeccf106de683708fd8b78483947f14
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-03 11:44:36 -0700

    Add test to return state with countries

--- src/reducers/countries.test.js
+++ src/reducers/countries.test.js
@@ -9,6 +9,35 @@ describe('countries', () => {
     expect(result).toEqual(expected)
   });
 
+  it('should return state with countries', () => {
+    const mockCountries = [
+        {
+          flag: "https://restcountries.eu/data/srb.svg",
+          name: "Serbia",
+          numericCode: 688,
+          region: "Europe"
+        },
+        {
+          flag: "https://restcountries.eu/data/lux.svg",
+          name: "Luxembourg",
+          numericCode: 442,
+          region: "Europe"
+        }
+    ]
+
+    const initialState = null;
+    const state = initialState;
+    const action = {
+      type: "SAVE_COUNTRIES",
+      countries: mockCountries
+    }
+
+    const newState = mockCountries;
+
+    const result = countries(state, action);
+
+    expect(result).toEqual(newState);
+  });
 
 
 

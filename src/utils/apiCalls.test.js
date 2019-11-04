23fb3ff4d52ede55fa009ba27be0bbc83329e11c
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-04 15:04:09 -0700

    Update apiCalls test per linter

--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -2,12 +2,11 @@ import { fetchData } from './apiCalls';
 import { mockData } from './mockData';
 
 describe('fetchData', () => {
-
   beforeEach(() => {
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
-        json: () => Promise.resolve(mockData)
+        json: () => Promise.resolve(mockData),
       });
     });
   });
@@ -16,14 +15,14 @@ describe('fetchData', () => {
     const mockUrl = 'https://restcountries.eu/rest/v2/all';
     await fetchData(mockUrl);
 
-    expect(window.fetch).toHaveBeenCalledWith(mockUrl)
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl);
   });
 
   it('should return an array of countries', async () => {
     const mockUrl = 'https://restcountries.eu/rest/v2/all';
 
     const results = await fetchData(mockUrl);
-    expect(results).toEqual(mockData)
+    expect(results).toEqual(mockData);
   });
 
   it('should return an error when promise resolved is not ok', () => {
@@ -31,7 +30,7 @@ describe('fetchData', () => {
       return Promise.resolve({
         ok: false,
         statusText: "Error. Could not fetch."
-      })
+      });
     });
 
     const mockUrl = 'https://kittens.com';
@@ -41,10 +40,9 @@ describe('fetchData', () => {
 
   it('should return an error if the server is down', () => {
     window.fetch = jest.fn().mockImplementation(() => ({
-      status: 500
+      status: 500,
     }))
 
     expect(fetchData()).rejects.toEqual(Error());
   });
- 
 });

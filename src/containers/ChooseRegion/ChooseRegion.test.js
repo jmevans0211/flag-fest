97cf73b2877edfe5ac6733930bc02c8eb249b810
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-04 11:54:56 -0700

    Add mapStateToProps test to ChooseRegion

--- src/containers/ChooseRegion/ChooseRegion.test.js
+++ src/containers/ChooseRegion/ChooseRegion.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { ChooseRegion } from './ChooseRegion';
+import { ChooseRegion, mapStateToProps, mapDispatchToProps } from './ChooseRegion';
 
 describe ('ChooseRegion', () => {
   let wrapper;
@@ -45,3 +45,32 @@ describe ('ChooseRegion', () => {
     expect(wrapper.instance().handleRegion).toHaveBeenCalledWith(mockEvent, 'europe')
   });
 });
+
+describe('mapStateToProps', () => {
+  it('should return an object with a countries array', () => {
+    const mockState = {
+      countries: [
+        {
+          flag: "https://restcountries.eu/data/srb.svg",
+          name: "Serbia",
+          numericCode: 688,
+          region: "Europe"
+        },
+        {
+          flag: "https://restcountries.eu/data/lux.svg",
+          name: "Luxembourg",
+          numericCode: 442,
+          region: "Europe"
+        }
+      ],
+      filter: 'SAVE_COUNTRIES'
+    };
+    const expected = {
+      countries: mockState.countries
+    }
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    });
+  });

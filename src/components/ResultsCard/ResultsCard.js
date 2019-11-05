62794f196155a7e402e308896d8a6555d656154c
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-05 09:56:14 -0700

    Add in conditional renedering to show error message;

--- src/components/ResultsCard/ResultsCard.js
+++ src/components/ResultsCard/ResultsCard.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './ResultsCard.scss'
 
-const ResultsCard = ({ points, flagsGuessed }) => {
+export const ResultsCard = ({ points, flagsGuessed, errorMessage }) => {
   const answers = flagsGuessed.map(flag => {
     return (
       <div key={flag.numericCode} className="correct-answer">
@@ -14,16 +15,27 @@ const ResultsCard = ({ points, flagsGuessed }) => {
   })
   return (
     <main className="results-card">
-      <h4 className="final-score">Score: {points}/{flagsGuessed.length}</h4>
-      <section className="correct-answers-display">Correct Answers: {answers}</section>
-      <Link className="play-again-link" to="/">
-        <p className="play-again" role="button">Play Again!</p>
-      </Link>
+      {errorMessage === '' && 
+      <>
+        <h4 className="final-score">Score: {points}/{flagsGuessed.length}</h4>
+        <section className="correct-answers-display">Correct Answers: {answers}</section>
+        <Link className="play-again-link" to="/">
+          <p className="play-again" role="button">Play Again!</p>
+        </Link>
+      </>
+      }
+      {errorMessage !== '' && 
+        <p>{errorMessage}</p>
+      }
     </main>
   )
 }
 
-export default ResultsCard;
+export const mapStateToProps = state => ({
+  errorMessage: state.errorMessage,
+});
+
+export default connect(mapStateToProps, null)(ResultsCard);
 
 ResultsCard.propTypes = {
   points: PropTypes.number.isRequired,

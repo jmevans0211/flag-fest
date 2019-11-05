b0be9916ab9903ab3ab0547b00d7e51da000f015
Jean-Marie Evans <jeanmarieevans3@gmail.com>
2019-11-05 10:13:12 -0700

    Add reducer test for errorMessage that state would change with an action

--- src/reducers/errorMessage.test.js
+++ src/reducers/errorMessage.test.js
@@ -8,4 +8,18 @@ describe('errorMessage', () => {
 
     expect(result).toEqual(expected);
   });
+
+  it('should return state with an error message', () => {
+    const mockMessage = 'Error, please try again.';
+    const initialState = null;
+    const action = {
+      type: 'HANDLE_ERROR',
+      errorMessage: mockMessage,
+    };
+    const newState = mockMessage;
+
+    const result = errorMessage(initialState, action);
+
+    expect(result).toEqual(newState);
+  });
 });
